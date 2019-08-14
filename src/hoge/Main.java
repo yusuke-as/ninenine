0ee874e5dc5119a7b875cc195ecd383ad941630a
yusuke <yusuke@192.168.2.108>
2019-08-14 10:42:13 +0900

    mainにて99をjavaを使用し動的に作成

---
+++ src/hoge/Main.java
@@ -0,0 +1,50 @@
+package hoge;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset='utf-8' />");
+		out.println("<title>9*9</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<table border=1");
+		for(int i=1;i<=9;i++) {
+			out.println("<tr>");
+			for(int j=1;j<=9;j++){
+				out.print("<td>");
+				out.print(i*j);
+				out.println("</td>");
+			}
+			out.println("</tr>");
+		}
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
